660e90fde9bf0b890871d1a6e00b5e188bdf9c1f
AnhTuFPT <tulnats01603@gmail.com>
2026-01-19 21:27:28 +0700

    Hoan thanh Lab 4 Bai 3

---
+++ Lab4/src/components/Bai3.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+
+    <button
+      :title="buttonTitle"
+      @click="changeGreeting"
+      class="btn btn-primary"
+    >
+      Click để thay đổi
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Xin chào bạn!',
+      buttonTitle: 'Nhấn để thay đổi lời chào'
+    }
+  },
+  methods: {
+    changeGreeting() {
+      this.greeting = 'Hello World!'
+      this.buttonTitle = 'Lời chào đã được cập nhật'
+    }
+  }
+}
+</script>
