a1269f4c24aef60d02fb93411754a58c2a47f0f6
AnhTuFPT <tulnats01603@gmail.com>
2026-01-27 07:16:48 +0700

    Hoan thanh Lab 5

---
+++ Lab5/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
+import Bai4 from './components/Bai4.vue';
+</script>
+
+<template>
+  <div class="container">
+    <h1>LAB 5</h1>
+    <!-- <Bai1 />
+    <Bai2 /> -->
+    <!-- <Bai3 /> -->
+    <Bai4 />
+  </div>
+</template>
+
