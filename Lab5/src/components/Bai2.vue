f6f7601653f7dd34e3978ffd5ff39fde354a0b2f
AnhTuFPT <tulnats01603@gmail.com>
2026-01-27 07:17:31 +0700

    Hoan thanh Lab 5 - Bai 2

---
+++ Lab5/src/components/Bai2.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+// Biến nhận dữ liệu từ input
+const userInput = ref('')
+</script>
+
+<template>
+  <div class="box">
+    <h2>Bài 2</h2>
+    <!-- Input dùng v-model -->
+    <input
+      v-model="userInput"
+      placeholder="Nhập dữ liệu vào đây"/>
+    <!-- Hiển thị giá trị realtime -->
+    <p>Giá trị bạn nhập: {{ userInput }}</p>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin-bottom: 20px;
+}
+input {
+  padding: 6px;
+  width: 100%;
+  margin-bottom: 10px;
+}
+</style>
