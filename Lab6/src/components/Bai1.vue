deaf472d5d08a5ecfaf9bfa66f9b3a64fc9f1da8
AnhTuFPT <tulnats01603@gmail.com>
2026-01-30 22:44:14 +0700

    Hoan thanh Lab 6 - Bai 1

---
+++ Lab6/src/components/Bai1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4 border rounded mb-4">
+    <h3>Bài 1</h3>
+
+    <input type="number" class="form-control w-50" v-model.number="dtb" placeholder=""/>
+
+    <p class="mt-3 fw-bold" v-if="dtb < 5">Xếp loại: Yếu</p>
+    <p class="mt-3 fw-bold" v-else-if="dtb < 6.5">Xếp loại: Trung bình</p>
+    <p class="mt-3 fw-bold" v-else-if="dtb < 8">Xếp loại: Khá</p>
+    <p class="mt-3 fw-bold" v-else-if="dtb < 9">Xếp loại: Giỏi</p>
+    <p class="mt-3 fw-bold" v-else>Xếp loại: Xuất sắc</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const dtb = ref(0)
+</script>
